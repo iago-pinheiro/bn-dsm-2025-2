7d3197aa165fccaedcd3d9e0a9704f1c4604c042
Iago Pinheiro <iagoo.pinheiro73@gmail.com>
2025-11-03 19:49:20 -0300

    (03/11) Término da implementação do back-end de referência

--- back-end/src/routes/pedidos.js
+++ back-end/src/routes/pedidos.js
@@ -15,4 +15,11 @@ router.put('/:id', controller.update)
 
 router.delete('/:id', controller.delete)
 
+// Rotas para os itens do pedido
+router.post('/:id/itens', controller.createItem)
+router.get('/:id/itens', controller.retrieveAllItems)
+router.get('/:id/itens/:itemId', controller.retrieveOneItem)
+router.put('/:id/itens/:itemId', controller.updateItem)
+router.delete('/:id/itens/:itemId', controller.deleteItem)
+
 export default router
